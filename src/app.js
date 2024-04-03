5014c0df1fb6f0e283d7e020e5416f8832f13973
Ayush-kathayat <kathayatayush789@gmail.com>
2024-04-03 20:10:26 +0530

    chore/backend : removed the node modules and the .md files from the githu

--- src/app.js
+++ src/app.js
@@ -3,10 +3,8 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import connectDB from "./configs/databaseConnection.js";   //dont forget to add the .js extension'
+import connectDB from "./configs/databaseConnection.js"; //dont forget to add the .js extension'
 import router from "./routes/tasksRoutes.js";
-
-
 
 const PORT = 5050;
 const app = express();
@@ -29,57 +27,9 @@ app.use(cookieParser());
 
 app.use("/api/v2", router); // using the routes
 
-
 // database connection
 
-connectDB();       // calling the function to connect to the database
-
-// app.get("/", (req, res) => {
-//   // console.log(req.cookies);
-//   res.send("Hello World");
-// });
-
-
-// app.post("/api/auth/signup", (req, res) => {
-//   const { username, email, password } = req.body;
-
-//   user
-//     .create({
-//       name: username,
-//       email: email,
-//       password: password,
-//     })
-//     .then(() => {
-//       res.cookie("token", "I AM Ayush", {
-//         sameSite: "lax",
-//         secure: false,
-//         httpOnly: true,
-//       });
-//       res.json({ message: "Form submitted successfully" });
-//     })
-//     .catch((err) => {
-//       console.error(err); // Log the error for debugging
-//       res.status(500).send("An error occurred during signup.");
-//     });
-// });
-// app.get("/api/auth/check-token", (req, res) => {
-//   const token = req.cookies["token"];
-
-//   console.log("Token:", token); // Log the token
-
-//   if (!token) {
-//     return res
-//       .status(401)
-//       .json({ message: "Hello i am from the server NOT AUTHENTICATED" });
-//   }
-
-//   // Here, you would typically verify the token and check if the user is authorized.
-//   // If the token is invalid or the user is not authorized, send a 401 or 403 response.
-
-//   // If the token is valid and the user is authorized, proceed with handling the request.
-// });
-
-// creating todos for the user
+connectDB(); // calling the function to connect to the database
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
