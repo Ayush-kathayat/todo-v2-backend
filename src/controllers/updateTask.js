d3508bc5e3faaffe99e394e0cf539c5e43343fbe
Ayush-kathayat <kathayatayush789@gmail.com>
2024-04-05 23:00:14 +0530

    chore/backend : added the updateTask controller function

--- src/controllers/updateTask.js
+++ src/controllers/updateTask.js
@@ -1,1 +1,21 @@
+import Task from "../models/tasks";
 
+const updateTask = async (req, res) => {
+  const { id: _id } = req.params;
+  const task = req.body;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      return res.status(404).send("No task with that id");
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      _id,                    
+      { ...task, _id },       //! destructuring the task and adding the same id to the task
+      { new: true }
+    );
+
+    res.json(updatedTask);
+  } catch (error) {
+    console.log(error.message || "Some error occurred while updating the Task");
+  }
+};
