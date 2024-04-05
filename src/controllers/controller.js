8c1bc493ef121aa8a31bc84d6224aeac13e9a258
Ayush-kathayat <kathayatayush789@gmail.com>
2024-04-05 15:22:00 +0530

    chore/backend : added the routes for the showTasks api

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,5 +2,6 @@
 
 import createTask from "./createTask.js";
 import deleteTask from "./deleteTask.js";
+import showTasks from "./showTasks.js";
 
-export { createTask, deleteTask };
+export { createTask, deleteTask, showTasks };
