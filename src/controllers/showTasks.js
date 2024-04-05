18676cfd0c9b1270045fc0c0297dd0bb9382c07f
Ayush-kathayat <kathayatayush789@gmail.com>
2024-04-05 14:52:53 +0530

    chore/backend : added the showTasks controller function

--- src/controllers/showTasks.js
+++ src/controllers/showTasks.js
@@ -1,2 +1,14 @@
 //! here we will import the model we created 
 
+import Task from '../models/tasks.js';
+
+const showTasks = async (req, res) => {
+  try{
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  }
+  catch(error){
+    console.log(error.message || "Some error occurred while fetching all the Tasks");
+  }
+}
+export default showTasks;
